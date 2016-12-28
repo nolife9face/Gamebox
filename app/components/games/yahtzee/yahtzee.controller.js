dd89bd989fbd6d620a803dc40ba7f2fb0c8dd8d9
Patrick Nolin <nolife9face@hotmail.com>
2016-12-27 23:23:17 -0500

    Added dices images. Showing dices instead of number with background image.

--- app/components/games/yahtzee/yahtzee.controller.js
+++ app/components/games/yahtzee/yahtzee.controller.js
@@ -16,13 +16,38 @@
 
         function rollDices(){
             for (var i = 0; i < 5; i++) {
-                vm.dices[i].value = Math.floor(Math.random() * 6) + 1  ;
+                vm.dices[i].value = Math.floor(Math.random() * 6) + 1;
+                assignClass(vm.dices[i]);
             }
         }
 
         function initialize(){
             for (var i = 0; i < 5; i++) {
                 vm.dices[i] = {value: i + 1};
+                assignClass(vm.dices[i]);
+            }
+        }
+
+        function assignClass(dice){
+            switch(dice.value){
+                case 1:
+                    dice.class = "dice-one";
+                    break;
+                case 2:
+                    dice.class = "dice-two";
+                    break;
+                case 3:
+                    dice.class = "dice-three";
+                    break;
+                case 4:
+                    dice.class = "dice-four";
+                    break;
+                case 5:
+                    dice.class = "dice-five";
+                    break;
+                case 6:
+                    dice.class = "dice-six";
+                    break;
             }
         }
     }
