2adf344b4f1ef0b304707515cb93051fe5a3f3cd
Patrick Nolin <nolife9face@hotmail.com>
2017-01-20 18:58:27 -0500

    Showing the yahtzee stats on the stats page.

--- app/components/stats/yahtzee/yahtzee-stats.controller.js
+++ app/components/stats/yahtzee/yahtzee-stats.controller.js
@@ -9,13 +9,16 @@
 
     function YahtzeeStatsController(serverApi) {
         var vm = this;
+        vm.yahtzeeStats = {};
 
         initialize();
 
         function initialize(){
             serverApi.get('http://localhost:8080/api/yahtzeeStats')
             .then(function(results){
-
+                vm.yahtzeeStats = results.data;
+                vm.yahtzeeStats.averageScore = Math.floor(vm.yahtzeeStats.averageScore);
+                vm.yahtzeeStats.bonusPercentage = Math.floor((vm.yahtzeeStats.numberOfBonus / vm.yahtzeeStats.totalGamePlayed) * 100);
             });
         }        
     }
