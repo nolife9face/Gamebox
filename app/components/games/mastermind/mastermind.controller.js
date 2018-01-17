ffa5ec288381ee5cba97cec0bd8868baa1b163fd
Patrick Nolin <patrick.nolin95@gmail.com>
2018-01-16 22:54:44 -0500

    Mastermind: Basic layout of the page.

--- app/components/games/mastermind/mastermind.controller.js
+++ app/components/games/mastermind/mastermind.controller.js
@@ -7,6 +7,60 @@
     
         function MastermindController() {
             var vm = this;
-            vm.text = 'mastermind';
+
+            var maxGuesses = 12;
+            var colorPerGuesses = 4;
+            var numberOfGuesses = 0;
+
+            vm.colors = [];
+            vm.guesses = [];
+
+            initialize();
+
+            function initialize() {
+                vm.colors = initializeColors();
+                initializeGuesses();
+            }
+
+            function initializeColors() {
+                return [
+                    {
+                        value: 0,
+                        name: 'red'
+                    },
+                    {
+                        value: 1,
+                        name: 'blue'
+                    },
+                    {
+                        value: 2,
+                        name: 'yellow'
+                    },
+                    {
+                        value: 3,
+                        name: 'green'
+                    },
+                    {
+                        value: 4,
+                        name: 'white'
+                    },
+                    {
+                        value: 5,
+                        name: 'black'
+                    }
+                ]
+            }
+
+            function initializeGuesses() {
+                var guess = {};
+                for (var i = 0; i < maxGuesses; i++) {
+                    guess.row = i;
+                    guess.colors = [];
+                    for (var j = 0; j < colorPerGuesses; j++) {
+                        guess.colors.push(vm.colors[0]);
+                    }
+                    vm.guesses.push(angular.copy(guess));
+                }
+            }
         }
     })();
